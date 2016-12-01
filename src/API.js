470182898dd2a940dbf2b9fd99cb88ae5799832a
Alexandre Segura <mex.zktk@gmail.com>
2016-12-01 11:12:55 +0100

    Cache categories for 10 minutes.

--- src/API.js
+++ src/API.js
@@ -1,11 +1,24 @@
+import CacheStore from 'react-native-cache-store';
+
 let Config = require('./Config');
 
 class API {
   static getCategories() {
-    return fetch(Config.API_BASEURL + '/categories')
-      .then((response) => response.json())
-      .then((json) => json['hydra:member'])
-      .catch((err) => {console.log('ERROR', err)});
+    return CacheStore.get('categories').then((categories) => {
+      if (!categories) {
+        console.log('Categories cache is expired or empty, fetching data from API...');
+        return fetch(Config.API_BASEURL + '/categories')
+          .then((response) => response.json())
+          .then((data) => {
+            console.log('Storing categories in cache');
+            let categories = data['hydra:member'];
+            return CacheStore.set('categories', categories, 10).then(() => categories);
+          })
+          .catch((err) => {console.log('ERROR', err)});
+      }
+
+      return categories;
+    });
   }
   static getObjects() {
     return fetch(Config.API_BASEURL + '/objects')
